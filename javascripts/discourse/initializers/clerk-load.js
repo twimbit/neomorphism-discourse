9ec9f0a0608e90c6d20d2d594b01ac84c195c95a
Siddhant <siddhant@twimbit.com>
2022-10-20 04:37:35 +0530

    clerk js added

---
+++ javascripts/discourse/initializers/clerk-load.js
@@ -0,0 +1,59 @@
+export default {
+    name: 'clerk',
+    initialize() {
+
+
+        const frontendApi = 'clerk.twimbit.com'; // <- Add Frontend API here
+
+        jQuery(document).ready(function ($) {
+            const script = document.createElement('script');
+            script.setAttribute('data-clerk-frontend-api', frontendApi);
+            script.async = true;
+            script.src = `https://${frontendApi}/npm/@clerk/clerk-js@2/dist/clerk.browser.js`;
+            script.addEventListener('load', startClerk);
+            script.addEventListener('error', () => {
+                document.getElementById('no-frontend-api-warning').hidden = false;
+            });
+            document.body.appendChild(script);
+
+        })
+
+
+        const startClerk = async () => {
+            const Clerk = window.Clerk;
+
+            try {
+                // Load Clerk environment and session if available
+                await Clerk.load();
+
+                $('.d-header-wrap').on('click', '.logout', () => {
+                    $.ajax({
+                        url: "https://auth.twimbit.com/logout?session=" + Clerk.session.id,
+                        type: "get",
+                        contentType: "application/json; charset=utf-8",
+                        dataType: "json",
+                    });
+                });
+
+                // Clerk.addListener(({client, user}) => {
+                //   if (!user) {
+                //     $.ajax({
+                //       url: "/clerk",
+                //       type: "post",
+                //       data: {data_value: JSON.stringify({status: "logout"})}
+                //     });
+                //   }
+                // });
+
+                // if (!Clerk.user) {
+                //
+                //   //document.location = 'http://localhost:4200';
+                // }
+
+            } catch (err) {
+                console.error('Error starting Clerk: ', err);
+            }
+        };
+    }
+};
+
